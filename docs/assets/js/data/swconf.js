95bdff549cb359b95fb53e0e423fd9e149055dc7
kun-1 <yguo719566@gmail.com>
2025-02-13 18:56:12 +0800

    Update URLs in documentation to use localhost for local development

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739174929',resources: [
+    cacheName: 'chirpy-1739195239',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
